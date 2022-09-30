9317f8aa05532e410a36b8b3bb65f8d66bb1b7d7
Hmmtrnt <rento5310@gmail.com>
2022-09-30 15:19:05 +0900

    進行方向

--- player.h
+++ player.h
@@ -5,6 +5,17 @@
 class Player
 {
 public:
+	// 定数定義
+
+	// プレイヤーグラフィック分割数
+	static constexpr int kGraphicDivX = 3;
+	static constexpr int kGraphicDivY = 4;
+	static constexpr int kGraphicDivNum = kGraphicDivX * kGraphicDivY;
+
+	// プレイヤーグラフィックサイズ
+	static constexpr int kGraphicSizeX = 32;
+	static constexpr int kGraphicSizeY = 32;
+public:
 	Player();
 	virtual ~Player();
 
@@ -20,13 +31,17 @@ public:
 	void draw();
 
 private:
-	int m_handle[12];
+	int m_handle[kGraphicDivNum];
 
 	// 表示位置
 	Vec2 m_pos;
 	// 移動
 	Vec2 m_vec;
 
+
+
 	// キャラクターのアニメーション
-	int m_animeNo;
+	int m_animeNo;		// 表示する番号
+	int m_animeFrame;
+	int m_dirNo;		// 進行方向
 };
